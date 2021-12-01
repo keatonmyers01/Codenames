077ec90d03c3980ab5aab667b6e59456e9ac2180
aheffne <78396431+aheffne@users.noreply.github.com>
2021-12-01 09:20:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
 #main file
+import random
+
+#makes and returns a 2d array containing a string of "blue", "red", "white", or "black"
+def make_key():
+    keyArr = [["white" for x in range(5)] for y in range(5)]
+    for x in range(9):
+        while True:
+            h = random.randint(0,4)
+            w = random.randint(0,4)
+            if(keyArr[h][w] == "white"):
+                keyArr[h][w] = "blue"
+                break
+    for y in range(8):
+        while True:
+            h = random.randint(0,4)
+            w = random.randint(0,4)
+            if(keyArr[h][w] == "white"):
+                keyArr[h][w] = "red"
+                break
+    while True:
+        h = random.randint(0,4)
+        w = random.randint(0,4)
+        if(keyArr[h][w] == "white"):
+            keyArr[h][w] = "black"
+            break
+
+
+    return keyArr
+##prints a 5x5 2d array as human readable for debugging
+def print2dArray(arr):
+    for x in range(5):
+        for y in range(5):
+            print(arr[x][y] + " ", end="")
+        print(" ")
